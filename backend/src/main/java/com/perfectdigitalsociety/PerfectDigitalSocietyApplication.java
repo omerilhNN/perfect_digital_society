84849858795ba5fbbd09fb36eb07676d2d293142
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2025-12-03 23:15:05 +0300

    backend directory hierarchy

---
+++ backend/src/main/java/com/perfectdigitalsociety/PerfectDigitalSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.perfectdigitalsociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectDigitalSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectDigitalSocietyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/perfectdigitalsociety/PerfectDigitalSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfectdigitalsociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectDigitalSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
